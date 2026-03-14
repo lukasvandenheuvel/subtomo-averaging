5fd3439ec890d7aa144e50edeaed1ab418e5fa77
Lukas van den Heuvel <lukas@jantijn.nl>
2026-03-14 12:56:04 +0100

    update paths in starfiles so that they are relative to relion project directory

--- warp_to_relion5.py
+++ warp_to_relion5.py
@@ -9,7 +9,7 @@ from scipy.spatial.transform import Rotation as R
 from utils import read_star, write_star
 
 
-def convert_star_to_relion5(warp_file, relion_file, tomo_file, output_file):
+def convert_star_to_relion5(warp_file, relion_file, tomo_file, output_file, output_file_relative):
     # Read star files
     warp_header, warp_df = read_star(warp_file)
 
@@ -26,6 +26,9 @@ def convert_star_to_relion5(warp_file, relion_file, tomo_file, output_file):
     tomo_sizeX = float(tomo_df['_rlnTomoSizeX'].iloc[0])
     tomo_sizeY = float(tomo_df['_rlnTomoSizeY'].iloc[0])
     tomo_sizeZ = float(tomo_df['_rlnTomoSizeZ'].iloc[0])
+
+    # To the tilt-series path, add the relative path to the extraction job
+    tomo_df['_rlnTomoTiltSeriesName'] = os.path.dirname(output_file_relative) + '/' + tomo_df['_rlnTomoTiltSeriesName']
 
     # assume column 1 is the “orig_filename” that embeds the particle number
     # relion_df['relion_particlenumber'] = pd.to_numeric(relion_df.iloc[:, 1], errors='coerce').astype('Int64')
@@ -48,16 +51,24 @@ def convert_star_to_relion5(warp_file, relion_file, tomo_file, output_file):
 
     # 5. UPDATE DATAFRAME
     relion_df[['_rlnAngleRot', '_rlnAngleTilt', '_rlnAnglePsi']] = new_subtomo_angles
+    # Rename the angle columns to match Relion 5 subtomogram naming convention
+    relion_df = relion_df.rename(columns={
+        '_rlnAngleRot': '_rlnTomoSubtomogramRot',
+        '_rlnAngleTilt': '_rlnTomoSubtomogramTilt',
+        '_rlnAnglePsi': '_rlnTomoSubtomogramPsi'
+    })
     relion_df['_rlnHelicalTrackLengthAngst'] = warp_df['_rlnHelicalTrackLengthAngst'].loc[idx].astype(float).values
     relion_df['_rlnAnglePsiFlipRatio'] = 0.5
     relion_df['_rlnAngleRotPrior'] = 0
     relion_df['_rlnAngleTiltPrior'] = 90
     relion_df['_rlnAnglePsiPrior'] = 0
     relion_df['_rlnHelicalTubeID'] = warp_df['_rlnHelicalTubeID'].loc[idx].astype(float).astype(int).values
-    relion_df['_rlnCenteredCoordinateXAngst'] = (warp_df['_rlnCoordinateX'].astype(float)-tomo_sizeX/2)
-    relion_df['_rlnCenteredCoordinateYAngst'] = (warp_df['_rlnCoordinateY'].astype(float)-tomo_sizeY/2)
-    relion_df['_rlnCenteredCoordinateZAngst'] = (warp_df['_rlnCoordinateZ'].astype(float)-tomo_sizeZ/2)
+    relion_df['_rlnCenteredCoordinateXAngst'] = (warp_df['_rlnCoordinateX'].loc[idx].astype(float)-tomo_sizeX/2)
+    relion_df['_rlnCenteredCoordinateYAngst'] = (warp_df['_rlnCoordinateY'].loc[idx].astype(float)-tomo_sizeY/2)
+    relion_df['_rlnCenteredCoordinateZAngst'] = (warp_df['_rlnCoordinateZ'].loc[idx].astype(float)-tomo_sizeZ/2)
     relion_df['_rlnRandomSubset'] = 0
+    # To the particle paths, add the relative path to the extraction job
+    relion_df['_rlnImageName'] = os.path.dirname(output_file_relative) + '/' + relion_df['_rlnImageName']
 
     # Assign random subset for each filament
     filamentIDs = relion_df['_rlnHelicalTubeID'].unique()
@@ -69,19 +80,30 @@ def convert_star_to_relion5(warp_file, relion_file, tomo_file, output_file):
     # Write the updated star file: remember there are 2 data_ blocks in the relion star file, 
     # we need to keep the first one unchanged and only update the second one
     write_star(output_file, relion_header_list, [relion_df_list[0],relion_df_list[1],relion_df])
+    
+    # Write the updated tomo star file: remember there are 2 data_ blocks in the tomo star file,
+    # we need to keep the second one unchanged and only update the first one
+    write_star(os.path.splitext(output_file)[0]+'_tomograms.star', tomo_header_list, [tomo_df, tomo_df_list[1]])
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert Warp star file to Relion5 format with angles, track length, and random subset.')
-    parser.add_argument('--root', required=True, help='Root path')
-    parser.add_argument('--warp', required=True, help='Path to warp star file')
-    parser.add_argument('--relion', required=True, help='Path to relion5 star file')
-    parser.add_argument('--tomo', required=True, help='Path to tomostar file')
-    parser.add_argument('--out', required=True, help='Path to output star file')
+    parser.add_argument('--relionroot', required=True, help='Path to the relion project directory')
+    parser.add_argument('--warp', required=True, help='Relative path to warp star file')
+    parser.add_argument('--relion', required=True, help='Relative path to relion5 star file')
+    parser.add_argument('--tomo', required=True, help='Relative path to tomostar file')
+    parser.add_argument('--out', required=True, help='Relative path to output star file')
     args = parser.parse_args()
 
-    warp_path = os.path.join(args.root, args.warp)
-    relion_path = os.path.join(args.root, args.relion)
-    tomo_path = os.path.join(args.root, args.tomo)
-    tomo_out = os.path.join(args.root, args.out)
-    convert_star_to_relion5(warp_path, relion_path, tomo_path, tomo_out)
+    warp_path = os.path.join(args.relionroot, args.warp)
+    relion_path = os.path.join(args.relionroot, args.relion)
+    tomo_path = os.path.join(args.relionroot, args.tomo)
+    tomo_out = os.path.join(args.relionroot, args.out)
+    convert_star_to_relion5(warp_path, relion_path, tomo_path, tomo_out, args.out)
+
+    # write a new optimisation set star file
+    opt_out = os.path.join(args.relionroot, os.path.splitext(args.out)[0]+'_optimisation_set.star')
+    with open(opt_out, 'w') as f:
+        f.write('\ndata_\n\n')
+        f.write(f'_rlnTomoParticlesFile\t{args.out}\n')
+        f.write(f'_rlnTomoTomogramsFile\t{os.path.splitext(args.out)[0]}_tomograms.star\n')
