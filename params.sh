a41962e3ce765808496e0f2b9ffea347da734c6b
Lukas van den Heuvel <lukas@jantijn.nl>
2026-08-13 16:51:04 +0200

    add reconstruction pixel size to parameters

--- params.sh
+++ params.sh
@@ -1,19 +1,26 @@
 #!/bin/bash  
-TOMONAME=Position_11_5
-ROOT=/mnt/storage/data/users/lukas/20251127_MSA_liftout/Session1/msa_human/20251127_lift_out/$TOMONAME
-#ROOT=/mnt/storage/data/users/lukas/20251127_MSA_liftout/Session2/Titan3_EPFL_LBEM_Lukas2_20251210/msa_human/20251127_lift_out/$TOMONAME
+
+### PARAMETERS ###
+
+TOMONAME=Position_70
+#ROOT=/mnt/storage/data/users/lukas/20251127_MSA_liftout/Session1/msa_human/20251127_lift_out/$TOMONAME
+ROOT=/mnt/storage/data/users/lukas/20251127_MSA_liftout/Session2/Titan3_EPFL_LBEM_Lukas2_20251210/msa_human/20251127_lift_out/$TOMONAME
 REPOSITORY=/mnt/storage/data/users/lukas/subtomo-averaging
-ANGPIX=1.98 # make sure it is a float, e.g. 2.00 (not 2)
-MAGNIFICATION=64000
-FRAMEEXPOSURE=3.4
-RECBINNING=4
-BOXSIZE=64
-TOMOSIZEX=4096
-TOMOSIZEY=4096
-TOMOSIZEZ=3000
+ANGPIX=1.98             # pixel size in Angs. Make sure it is a float, e.g. 2.00 (not 2)
+MAGNIFICATION=64000     # nominal magnification
+FRAMEEXPOSURE=3.4       # exposure per frame in e/A2
+RECBINNING=4            # binning factor for reconstruction (choose something nice for picking particles)
+BOXSIZE=64              # box size for inital subtomogram extraction (in pixels)
+TOMOSIZEX=4096          # pixels
+TOMOSIZEY=4096          # pixels
+TOMOSIZEZ=3000          # pixels
 MOTIONCOR_EXECUTABLE=/programs/x86_64-linux/system/sbgrid_bin/MotionCor2_1.6.4_Cuda112_Mar312023
+
+### END PARAMETERS ###
+
 ANGPIX_BIN2=$(echo "$ANGPIX * 2" | bc)
 ANGPIX_BIN4=$(echo "$ANGPIX * 4" | bc)
+ANGPIX_REC=$(echo "$ANGPIX * $RECBINNING" | bc)
 
 echo "TOMONAME: $TOMONAME"
 echo "ROOT: $ROOT"
@@ -29,3 +36,4 @@ echo "TOMOSIZEZ: $TOMOSIZEZ"
 echo "MOTIONCOR_EXECUTABLE: $MOTIONCOR_EXECUTABLE"
 echo "ANGPIX_BIN2: $ANGPIX_BIN2"
 echo "ANGPIX_BIN4: $ANGPIX_BIN4"
+echo "ANGPIX_REC: $ANGPIX_REC"
